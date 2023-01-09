1819b0197ab95386f8de06610616dd8777731cf4
mattlockyer <mattdlockyer@gmail.com>
2023-01-09 15:23:04 -0800

    add back promise check refund if account cannot be created

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LookupSet};
+use near_sdk::json_types::{U128};
 use near_sdk::{
-    env, require, near_bindgen, PanicOnDefault, AccountId, Promise, PublicKey,
+    env, ext_contract, require, near_bindgen, PanicOnDefault, AccountId, Promise, PromiseResult, Gas, PublicKey,
 };
+
+/// Gas attached to the callback from account creation.
+pub const ON_CREATE_ACCOUNT_CALLBACK_GAS: Gas = Gas(20_000_000_000_000);
+
+#[ext_contract(ext_self)]
+pub trait ExtLinkDrop {
+    /// Callback after plain account creation.
+    fn on_account_created(&mut self, predecessor_account_id: AccountId, amount: U128) -> bool;
+}
+
+fn is_promise_success() -> bool {
+    assert_eq!(
+        env::promise_results_count(),
+        1,
+        "Contract expected a result on the callback"
+    );
+    match env::promise_result(0) {
+        PromiseResult::Successful(_) => true,
+        _ => false,
+    }
+}
 
 #[near_bindgen]
 #[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
@@ -40,6 +62,29 @@ impl AccountFactory {
             .create_account()
             .add_full_access_key(new_public_key.into())
             .transfer(amount)
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(ON_CREATE_ACCOUNT_CALLBACK_GAS)
+                    .on_account_created(
+                        env::predecessor_account_id(),
+                        amount.into()
+                    )
+            )
+    }
+
+    /// Callback after executing `create_account`.
+    pub fn on_account_created(&mut self, predecessor_account_id: AccountId, amount: U128) -> bool {
+        assert_eq!(
+            env::predecessor_account_id(),
+            env::current_account_id(),
+            "Callback can only be called from the contract"
+        );
+        let creation_succeeded = is_promise_success();
+        if !creation_succeeded {
+            // In case of failure, send funds back.
+            Promise::new(predecessor_account_id).transfer(amount.into());
+        }
+        creation_succeeded
     }
 
     /// approved creators mgmt
